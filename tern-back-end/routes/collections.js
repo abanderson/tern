e96b699ab4afdea4a2323c73d7cc77cd3f0a3d83
Alex Anderson <alex@abanderson.com>
2018-08-15 16:19:11 -0400

    Added GET routes

--- tern-back-end/routes/collections.js
+++ tern-back-end/routes/collections.js
@@ -4,9 +4,43 @@ const models = require("../models");
 
 /* GET all collections */
 router.get("/", function(req, res, next) {
-    models.collection.findAll().then(collection => {
-        res.json(collection);
-    });
+    models.collection
+        .findAll()
+        .then(collections => {
+            res.json(collections);
+        })
+        .catch(err => {
+            next(err);
+        });
+});
+
+/* GET all collections for a specific author (user) */
+router.get("/:authorid", (req, res, next) => {
+    models.collection
+        .findAll({ where: { authorId: req.params.authorid } })
+        .then(collections => {
+            res.json(collections);
+        })
+        .catch(err => {
+            next(err);
+        });
+});
+
+/* GET one collection by name for a specific author (user) */
+router.get("/:authorid/:name", (req, res, next) => {
+    models.collection
+        .findOne({
+            where: {
+                name: { $iLike: req.params.name },
+                authorId: req.params.authorid
+            }
+        })
+        .then(collection => {
+            res.json([collection]);
+        })
+        .catch(err => {
+            next(err);
+        });
 });
 
 module.exports = router;
