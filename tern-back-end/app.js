36957c46aa55ec4286892aee5ba22252cc9ccfd6
Alex Anderson <alex@abanderson.com>
2018-08-14 21:19:07 -0400

    Added routes

--- tern-back-end/app.js
+++ tern-back-end/app.js
@@ -4,26 +4,11 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-const models = require("./models");
-
-// const indexRouter = require("./routes/index");
-// const usersRouter = require("./routes/users");
-
-models.entry.findOne().then(entry => {
-    console.log(entry);
-});
-
-models.author.findOne().then(author => {
-    console.log(author);
-});
-
-models.collection.findOne().then(collection => {
-    console.log(collection);
-});
-
-models.log.findOne().then(log => {
-    console.log(log);
-});
+// Define routes
+const authorsRouter = require("./routes/authors");
+const collectionsRouter = require("./routes/collections");
+const entriesRouter = require("./routes/entries");
+const logsRouter = require("./routes/logs");
 
 const app = express();
 
@@ -33,8 +18,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use("/", indexRouter);
-// app.use("/users", usersRouter);
+// Use routes
+app.use("/authors", authorsRouter);
+app.use("/collections", collectionsRouter);
+app.use("/entries", entriesRouter);
+app.use("/logs", logsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
